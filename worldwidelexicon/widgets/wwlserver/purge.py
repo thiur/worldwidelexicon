952ef7f495fe025ee8e06efec061a3109cfa986b
bsmcconnell <bsmcconnell@ed130684-8344-0410-a830-270b178aa175>
2010-03-29 22:45:13 +0000

    Now automatically purges log entries after 90 days.

--- worldwidelexicon/widgets/wwlserver/purge.py
+++ worldwidelexicon/widgets/wwlserver/purge.py
@@ -3,39 +3,21 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from geo import geo
+from database import Search
 
-class Translation(db.Model):
-    sl = db.StringProperty(default='')
-    
-class Objects(db.Model):
-    title = db.StringProperty(default='')
-    
-class GeoLog(db.Model):
-    date = db.DateTimeProperty()
-
-class GeoDB(db.Model):
-    country = db.TextProperty()
-
-class purge(webapp.RequestHandler):
-    def get(self, name=""):
-        if name == 'Objects':
-            wdb = db.Query(Objects)
-        elif name == 'GeoLog':
-            result = geo.purge()
-            self.response.out.write('ok')
-            return
-        elif name == 'GeoDB':
-            wdb = db.Query(GeoDB)
-            wdb.filter('country = ', '')
+class Purge(webapp.RequestHandler):
+    def get(self, name=''):
+        if name == 'search':
+            result = Search.purge()
+        elif name == 'geodb':
+            result = geo.purge(name='geodb')
         else:
-            wdb = db.Query(GeoLog)
-            wdb.order('date')
-        results = wdb.fetch(limit=500)
-        if results is not None:
-            db.delete(results)
+            result = geo.purge()
         self.response.out.write('ok')
 
-application = webapp.WSGIApplication([(r'/purge/(.*)', purge)],
+application = webapp.WSGIApplication([('/purge', Purge),
+                                      ('/heartbeat', Purge),
+                                      (r'/purge/(.*)', Purge)],
                                      debug=True)
 
 def main():
