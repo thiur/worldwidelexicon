cf00dae13b00f3203483763e631d50534faf664e
bsmcconnell <bsmcconnell@ed130684-8344-0410-a830-270b178aa175>
2010-04-01 20:27:02 +0000

    Added registration service for Word Press translator, system tracks recently published and translated wordpress posts.

--- worldwidelexicon/widgets/wwlserver/hosts.py
+++ worldwidelexicon/widgets/wwlserver/hosts.py
@@ -55,9 +55,11 @@ import string
 import datetime
 import codecs
 import urllib
+import md5
 # import WWL and third party modules
 import demjson
 from database import Users
+from www import www
 
 class Hosts(db.Model):
     domain = db.StringProperty(default='')
@@ -74,6 +76,168 @@ class Hosts(db.Model):
     planlevel = db.StringProperty(default='free')
     username = db.StringProperty(default='')
     lastupdated = db.DateTimeProperty(auto_now_add = True)
+
+class DirectoryIP(db.Model):
+    remote_addr = db.StringProperty(default='')
+    domains = db.ListProperty(str)
+    apikey = db.StringProperty(default='')
+    @staticmethod
+    def verify(remote_addr,domain):
+        if len(remote_addr) > 0 and len(domain) > 0:
+            hdb = db.Query(DirectoryIP)
+            hdb.filter('remote_addr = ', remote_addr)
+            item = hdb.get()
+            if item is not None:
+                domains = item.domains
+                if len(domains) > 5:
+                    return False
+                else:
+                    if domain in domains:
+                        return True
+                    else:
+                        domains.append(domain)
+                        item.domains = domains
+                        item.put()
+                        return True
+            else:
+                item = DirectoryIP()
+                item.remote_addr = remote_addr
+                domains = list()
+                domains.append(domain)
+                item.domains = domains
+                item.put()
+                return True
+        else:
+            return False
+
+class Directory(db.Model):
+    guid = db.StringProperty(default='')
+    domain = db.StringProperty(default='')
+    url = db.StringProperty(default='')
+    sl = db.StringProperty(default='')
+    tl = db.StringProperty(default='')
+    title = db.StringProperty(default='')
+    ttitle = db.StringProperty(default='')
+    description = db.TextProperty(default='')
+    tdescription = db.TextProperty(default='')
+    tags = db.ListProperty(str)
+    views = db.IntegerProperty(default=0)
+    lastupdated = db.DateTimeProperty(auto_now_add=True)
+    hosttype = db.StringProperty(default='')
+    indexed = db.BooleanProperty(default=False)
+    words = db.ListProperty(str)
+    @staticmethod
+    def purge():
+        td = datetime.timedelta(days=-30)
+        lastdate = datetime.datetime.now() + td
+        hdb = db.Query(Directory)
+        hdb.filter('lastupdated < ', lastdate)
+        results = hdb.fetch(limit=200)
+        if len(results) > 0:
+            db.delete(results)
+            return True
+        else:
+            return False
+    @staticmethod
+    def save(remote_addr, domain, url, sl, tl, title, ttitle='', description='', tdescription='', tags='', hosttype=''):
+        if len(domain) > 0 and len(url) > 0 and len(sl) > 0 and len(tl) > 0:
+            valid = DirectoryIP.verify(remote_addr, domain)
+            if valid:
+                m = md5.new()
+                m.update(url)
+                m.update(sl)
+                m.update(tl)
+                guid = str(m.hexdigest())
+                hdb = db.Query(Directory)
+                hdb.filter('guid = ', guid)
+                item = hdb.get()
+                if item is None:
+                    item = Directory()
+                    item.guid = guid
+                    item.sl = sl
+                    item.tl = tl
+                    item.domain = domain
+                    item.url = url
+                    item.hosttype = hosttype
+                item.title = title
+                item.description = description
+                item.ttitle = ttitle
+                item.tdescription = tdescription
+                if len(tags) > 0:
+                    tags = string.lower(tags)
+                    tags = string.split(tags, ',')
+                    if len(tags) > 0:
+                        item.tags = tags
+                item.lastupdated = datetime.datetime.now()
+                item.put()
+                return True
+            else:
+                return False
+        else:
+            return False
+
+class HostsSubmit(webapp.RequestHandler):
+    """
+    /hosts/submit
+
+    This request handler is used to process submissions from
+    Word Press and other plugins, to build a directory of sites
+    and recent publishing activity. It is called once per page
+    load on the remote system. It expects the following parameters:
+
+    domain = domain of your website
+    url = permalink of the page being viewed
+    sl = source language code
+    tl = target language code
+    title = page title
+    ttitle = translated page title
+    description = page description or initial block of body text
+    tdescription = translated page description
+    tags = tags (optional)
+    apikey = API key (to unlock multiple domains per IP address
+                otherwise, only allows one domain per IP address)
+
+    Returns ok or error message (only if API key is invalid, otherwise
+    it does not reveal if it accepted or rejected the submission (i.e.
+    flagged as spam by Akismet)
+    """
+    def get(self):
+        self.requesthandler()
+    def post(self):
+        self.requesthandler()
+    def requesthandler(self):
+        remote_addr = self.request.remote_addr
+        domain = self.request.get('domain')
+        url = self.request.get('url')
+        sl = self.request.get('sl')
+        tl = self.request.get('tl')
+        title = self.request.get('title')
+        ttitle = self.request.get('ttitle')
+        description = self.request.get('description')
+        tdescription = self.request.get('tdescription')
+        tags = self.request.get('tags')
+        apikey = self.request.get('apikey')
+        hosttype = self.request.get('hosttype')
+        if len(domain) > 0 and len(url) > 0 and len(sl) > 0 and len(tl) > 0:
+            if string.count(url, 'http://') < 1 and string.count(url, 'https://') < 1:
+                url = 'http://' + url
+            result=Directory.save(remote_addr,domain,url,sl,tl,title,ttitle=ttitle,description=description,tdescription=tdescription,tags=tags,hosttype=hosttype)
+            self.response.out.write('ok')
+        else:
+            www.serve(self,self.__doc__)
+            self.response.out.write('<table><form action=/hosts/submit method=get>')
+            self.response.out.write('<tr><td>Domain</td><td><input type=text name=domain></td></tr>')
+            self.response.out.write('<tr><td>URL</td><td><input type=text name=url></td></tr>')
+            self.response.out.write('<tr><td>Source Language Code</td><td><input type=text name=sl></td></tr>')
+            self.response.out.write('<tr><td>Target Language Code</td><td><input type=text name=tl></td></tr>')
+            self.response.out.write('<tr><td>Title</td><td><input type=text name=title></td></tr>')
+            self.response.out.write('<tr><td>Description</td><td><input type=text name=description></td></tr>')
+            self.response.out.write('<tr><td>Translated Title</td><td><input type=text name=ttitle></td></tr>')
+            self.response.out.write('<tr><td>Translated Description</td><td><input type=text name=tdescription></td></tr>')
+            self.response.out.write('<tr><td>Tags</td><td><input type=text name=tags></td></tr>')
+            self.response.out.write('<tr><td>Host Type (e.g. wordpress)</td><td><input type=text name=hosttype></td></tr>')
+            self.response.out.write('<tr><td>API Key</td><td><input type=text name=apikey></td></tr>')
+            self.response.out.write('<tr><td colspan=2><input type=submit value="Submit"></td></tr></table></form>')
 
 class HostsAuth(webapp.RequestHandler):
     """
@@ -156,6 +320,7 @@ class HostsUpdate(webapp.RequestHandler):
         pass
       
 application = webapp.WSGIApplication([(r'/hosts/auth/(.*)', HostsAuth),
+                                      ('/hosts/submit', HostsSubmit),
                                       ('/hosts/log', HostsLog)],
                                      debug=True)
 
